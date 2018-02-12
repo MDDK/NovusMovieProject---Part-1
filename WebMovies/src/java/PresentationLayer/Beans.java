5bb24a58825888c55de7021a287dc3f0354dffdc
Marek Dudek <mdudek15@gmail.com>
2018-02-12 16:37:17 +0000

    update

--- WebMovies/src/java/PresentationLayer/Beans.java
+++ WebMovies/src/java/PresentationLayer/Beans.java
@@ -42,7 +42,7 @@ public class Beans extends BaseBean implements Serializable{
         }
     }
     
-    // populate dropdown lists with ALL data retrieved
+    //populate  dropdown lists with ALL data retrieved 
     private void populateDropDownsWithOriginalData(){
         try{
             Films films = mbl.getFilms();
